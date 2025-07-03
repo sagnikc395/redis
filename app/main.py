2cfb6eb8f8c9db0e3b47165fa54c5c5941147f55
sagnikc395 <sagnikchatterjee607@gmail.com>
2025-07-04 01:08:48 +0530

    feat: add support for multiple concurrent clients

--- app/main.py
+++ app/main.py
@@ -1,5 +1,14 @@
 import socket  # noqa: F401
+import threading
 
+BUFFER_SIZE = 4096
+
+# walrus operator
+def handlePING(client: socket.socket):
+    while chunk := client.recv(BUFFER_SIZE):
+        if chunk == b"":
+            break
+        client.sendall(b"+PONG\r\n")
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -8,22 +17,24 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    conn, addr = server_socket.accept() # wait for client
+    # conn, addr = server_socket.accept() # wait for client
     # print(f"Accepted connection from {addr}")
     # send the response to PING
     # conn.sendall(b"+PONG\r\n")
+    # while True:
+    #     data = conn.recv(1024)
+    #     if not data:
+    #         break
+    #     lines = data.decode().split("\n")
+    #     for line in lines:
+    #         cmd = line.strip().upper()
+    #         if cmd == "PING":
+    #             conn.sendall(b"+PONG\r\n")
     while True:
-        data = conn.recv(1024)
-        if not data:
-            break
-        lines = data.decode().split("\n")
-        for line in lines:
-            cmd = line.strip().upper()
-            if cmd == "PING":
-                conn.sendall(b"+PONG\r\n")
+        client_socket, client_addr = server_socket.accept()
+        threading.Thread(target=handlePING,args=(client_socket,)).start()
 
-    conn.close()
-    server_socket.close()
+
 
 
 
